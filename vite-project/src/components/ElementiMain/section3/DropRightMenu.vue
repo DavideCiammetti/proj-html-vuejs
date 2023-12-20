7ee6baaebd1da8a887f1759ecb2036bbdd58cb96
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-20 15:57:16 +0100

    fix: drop right menu e button fine section 3

---
+++ vite-project/src/components/ElementiMain/section3/DropRightMenu.vue
@@ -0,0 +1,55 @@
+<script>
+
+export default{
+    name: 'DropRigth',
+}
+</script>
+
+<template>
+    <div class="right-att">
+        <ul class="list-content">
+            <li class="ico-drop-list p-15">
+                <a href="#"><font-awesome-icon :icon="['fas', 'desktop']" /></a>
+            </li>
+            <li class="ico-drop-list">
+                <a href="#"><font-awesome-icon :icon="['fas', 'life-ring']" /></a>
+            </li>
+            <li class="ico-drop-list">
+                <a href="#"><font-awesome-icon :icon="['fas', 'wrench']" /></a>
+            </li>
+            <li class="ico-drop-list">
+                <a href="#"><font-awesome-icon :icon="['fas', 'cart-shopping']" /></a>
+            </li>
+            <li class="ico-drop-list">
+                <a href="#"><font-awesome-icon :icon="['fas', 'mobile']" /></a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/_variables.scss' as *;
+
+.right-att{
+    position: absolute;
+    top: 77%;
+    right: 0;
+    box-shadow: -1px 3px 6px #d1d1d1;
+        .list-content{
+            list-style: none;
+            text-align: center;
+        }
+        .ico-drop-list{
+            padding-bottom:21px;
+            font-size: 20px;
+            padding-left: 19px;
+            padding-right: 19px;
+        }
+        .p-15{
+            padding-top: 15px;
+        }
+        a{
+            color: #616161;
+        }
+}
+</style>
--- vite-project/src/components/ElementiMain/section3/SectionThree.vue
+++ vite-project/src/components/ElementiMain/section3/SectionThree.vue
@@ -1,12 +1,14 @@
 <script>
 import MatrialsList from './MatrialsList.vue';
 import MaterialsGrid from './MaterialsGrid.vue';
+import DropRightMenu from './DropRightMenu.vue';
 
    export default {
         name:'SectioThree',
             components:{
                 MatrialsList,
                 MaterialsGrid,
+                DropRightMenu,
             }
     }
 
@@ -19,17 +21,34 @@ import MaterialsGrid from './MaterialsGrid.vue';
         </div>
         <MatrialsList></MatrialsList>
         <MaterialsGrid></MaterialsGrid>
+        <DropRightMenu></DropRightMenu>
+        <div class="but-container">
+            <input type="button" class="butt" value="SHOW ALL">
+        </div>
     </section>
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/_variables.scss' as *;
     #section-three{
         display: flex;
         flex-direction: column;
         align-items: center;
+        position: relative;
             h3{
                 font-size: 2.8125rem;
                 font-weight: 100;
+            }
+            .but-container{
+                margin: 80px 0 109px 0;
+                .butt{
+                padding: 14px 23px;
+                border: none;
+                outline: none;
+                background-color: $blue-600-search;
+                border-radius: 35px;
+                color: white;
+                }
             }
     }
 </style>
